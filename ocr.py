d4b6977f598c25bb9bc51937dc89d11ffac41e19
ukran1um <ukran1um@users.noreply.github.com>
2023-12-10 10:04:02 -0500

    styling and fixes

--- ocr.py
+++ ocr.py
@@ -5,15 +5,36 @@ import os
 import base64
 import requests
 from PIL import Image
-from utils import calculate_image_token_cost
+from utils import calculate_image_token_cost, get_index_results
+import openai
+import json
+from sentence_transformers import SentenceTransformer
+import hnswlib
 
-
-detail = "high"
-
+#set params and variables
+DETAIL = "high"
+MODEL =  "gpt-4"
+EMBEDDING_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
 api_key = os.environ['OPENAI_API_KEY']
 
-st.title('Diagram Extractor')
-st.subheader('Use Public Data Only')
+#load model
+model = SentenceTransformer(EMBEDDING_MODEL)
+
+st.set_page_config(layout="wide")
+
+# Logo configuration
+logo_path = "./data/logo2.jpeg"  # Path to your logo image file
+logo = Image.open(logo_path)
+
+# Create columns for logo and title
+col1, col2 = st.columns([1, 4])  # Adjust the ratio based on your preference
+
+with col1:
+    st.image(logo, width=250)  # Adjust width to fit your logo
+
+with col2:
+    st.title('Diagram Extractor')
+    st.subheader('Use Public Data Only')
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
@@ -27,59 +48,124 @@ if uploaded_file is not None:
     base64_image = base64.b64encode(bytes_data).decode("utf-8")
 
     
-    # load prompt
-    with open('prompt.txt', 'r') as file:
-        prompt = file.read()
+    # load prompts
+    with open('prompt1.txt', 'r') as file:
+        PROMPT1 = file.read()
 
-    # Use OpenAI's API to extract information from the image
-    try:
-        
-        headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-        }
+    with open('prompt2.txt', 'r') as file:
+        PROMPT2 = file.read()
 
-        payload = {
-        "model": "gpt-4-vision-preview",
-        # "response_format":{ "type": "json_object" },
-        "messages": [
-            {
-            "role": "user",
-            "content": [
+    with open('prompt3.txt', 'r') as file:
+        PROMPT3 = file.read()
+    print("1")
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("UNSPSC Data Lookup")
+        ### Use OpenAI's API to get the image name and image description for USNPSC code lookup
+        try:
+            
+            messages= [
                 {
-                "type": "text",
-                "text": prompt
-                },
-                {
-                "type": "image_url",
-                "image_url": {
-                    "url": f"data:image/jpeg;base64,{base64_image}",
-                    "detail": detail
+                "role": "user",
+                "content": [
+                    {"type": "text",  "text": PROMPT2},
+                    {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                        "detail": DETAIL
+                    }
+                    }
+                ]
                 }
+            ],
+            response = openai.ChatCompletion.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                max_tokens = 4096,
+                temperature = 0
+                # response_format={ "type": "json_object" }
+                )
+            print("2")
+            # st.write(response.json())
+            resp_text2 = response['choices'][0]['message']["content"]
+            print(resp_text2)
+            # resp_json = json.loads(resp_text2)
+            # print(resp_json)
+            #embed input
+            string_vector = model.encode([resp_text2])
+            print(string_vector)
+            results = get_index_results(string_vector)
+            print(results)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+        ### Use OpenAI's API to get the best matching UNSPNC code input.
+        try:
+            
+            messages= [
+                {
+                "role": "system",
+                "content": PROMPT3 + resp_text2 +"\nAnd here is are the potential USNPSC matches:\n"+results
                 }
             ]
-            }
-        ],
-        "max_tokens": 4096
-        }
 
-        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-        # st.write(response.json())
-        resp_text = response.json()['choices'][0]['message']["content"]
+            response = openai.ChatCompletion.create(
+                model=MODEL,
+                messages=messages,
+                max_tokens = 4096,
+                temperature = 0
+                )
+            
+            # st.write(response.json())
+            resp_text3 = response['choices'][0]['message']["content"]
+            st.markdown(resp_text3)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
-        #calculate costs
-        image_tokens = calculate_image_token_cost(image, detail)
-        response_tokens = len(resp_text)/4.0
-        input_price = .01
-        output_price = .03
+    with col2:
+        st.header("Data Extraction")
+        ### Use OpenAI's API to extract information from the image and structure it
+        try:
+            
+            messages= [
+                {
+                "role": "user",
+                "content": [
+                    {"type": "text",  "text": PROMPT1},
+                    {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                        "detail": DETAIL
+                    }
+                    }
+                ]
+                }
+            ],
+            response = openai.ChatCompletion.create(
+                model="gpt-4-vision-preview",
+                messages=messages,
+                max_tokens = 4096,
+                temperature = 0
+                )
+            
+            # st.write(response.json())
+            resp_text = response['choices'][0]['message']["content"]
 
-        cost = round(image_tokens*input_price/1000 + response_tokens*output_price/1000,2)*100
+            #calculate costs
+            image_tokens = calculate_image_token_cost(image, DETAIL)
+            response_tokens = len(resp_text)/4.0
+            input_price = .01
+            output_price = .03
 
-        st.markdown(f"Approximate cost: :green[***{cost:.2f} cents***]")
+            cost = round(image_tokens*input_price/1000 + response_tokens*output_price/1000,2)*100
 
-        st.markdown(resp_text)
-        
+            # st.markdown(f"Approximate cost: :green[***{cost:.2f} cents***]")
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+            st.markdown(resp_text)
+            
 
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
